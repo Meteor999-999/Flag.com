01d21406e89b125bb6004f1c0e8420ea2c470d2e
Meteor999-999 <hishamboghdady6@gmail.com>
2024-11-02 07:48:17 -0700

    Add files via upload

---
+++ scriptApp20.js
@@ -0,0 +1,16 @@
+window.onscroll = () => {
+    
+    document.querySelector(".mountains").style.marginTop = scrollY / 2 + "px";
+
+    document.querySelector(".stars").style.marginBottom = scrollY / 2.5 + "px";
+
+    document.querySelector(".stars").style.marginLeft = -scrollY / 2 + "px";
+
+    document.querySelector(".cave").style.transform = "scale(" + (scrollY / 5000 + 1) + ")";
+
+    document.querySelector(".moon").style.transform = "rotate(-" + (scrollY / 5.5) + "deg)";
+
+    document.querySelector(".Time-Element h1").style.marginTop = scrollY / 1.3 + "px";
+
+    document.querySelector(".Time-Element h1").style.opacity = (200 - scrollY) / 200;
+}
